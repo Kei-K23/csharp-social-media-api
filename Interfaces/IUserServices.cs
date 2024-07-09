9045485ba1f887cdc4fdf4d78391fb1ad2aac6f3
Kei-K23 <keiksl2301@gmail.com>
2024-07-09 13:44:12 +0630

    Add interface for user service

---
+++ Interfaces/IUserServices.cs
@@ -0,0 +1,14 @@
+using SocialMediaAPI.Contracts.User;
+using SocialMediaAPI.Models;
+
+namespace SocialMediaAPI.Interfaces
+{
+    public interface IUserServices
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync();
+        Task CreateUserAsync(UserRequest request);
+        Task UpateUserAsync(Guid id, UserRequest request);
+        Task DelteUserAsync(Guid id);
+    }
+}
